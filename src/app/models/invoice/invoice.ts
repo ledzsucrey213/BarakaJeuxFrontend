05eb5fca20e64bef4ff9501a6d8c917e0807e123
Yanis Baroudi <yanis.baroudi@etu.umontpellier.fr>
2024-12-07 17:48:01 +0100

    invoices

--- src/app/models/invoice/invoice.ts
+++ src/app/models/invoice/invoice.ts
@@ -1,22 +1,30 @@
-// models/invoice/invoice.ts
+import { Sale } from "../sale/sale";
+
 export class Invoice {
-    _id: string;
-    sale_id: string;
-    buyer_id: string;
-  
-    constructor(data: Partial<Invoice>) {
-      this._id = data._id || '';
-      this.sale_id = data.sale_id || '';
-      this.buyer_id = data.buyer_id || '';
-    }
-  
-    // Méthode pour créer une instance d'Invoice à partir d'un objet JSON
-    static createFrom(json: any): Invoice {
-      return new Invoice({
-        _id: json._id,
-        sale_id: json.sale_id,
-        buyer_id: json.buyer_id,
-      });
-    }
+  _id: string;
+  sale_id: Sale; // Objet Sale
+  buyer_id: string;
+
+  constructor(
+    _id: string,
+    sale_id: Sale,
+    buyer_id: string
+  ) {
+    this._id = _id;
+    this.sale_id = sale_id;
+    this.buyer_id = buyer_id;
   }
-  
+
+  /**
+   * Méthode pour créer une instance d'Invoice à partir d'un objet JSON
+   * @param json - Données en format brut (JSON)
+   * @returns Instance d'Invoice
+   */
+  static createFrom(json: any): Invoice {
+    return new Invoice(
+      json._id || '',
+      Sale.createFrom(json.sale_id), // Convertit les données brutes en instance de Sale
+      json.buyer_id || ''
+    );
+  }
+}
