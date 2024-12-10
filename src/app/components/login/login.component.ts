3014e81529c10a23c09a3cb2ce226f7fa23e8ca3
Yanis Baroudi <yanis.baroudi@etu.umontpellier.fr>
2024-12-10 22:12:20 +0100

    ahahahaha

---
+++ src/app/components/login/login.component.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { truncateSync } from 'fs';
+
+@Component({
+  selector: 'app-login',
+  imports: [],
+  templateUrl: './login.component.html',
+  styleUrl: './login.component.scss',
+  standalone : true
+})
+export class LoginComponent {
+
+  login(): void {
+    
+  }
+
+}
